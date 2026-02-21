5cb49afa984d9e6006b059bb2bb5f48cba46ef43
hanumantghub <hanumantg@gmail.com>
2026-02-22 00:01:46 +0530

    smallest and biggest change

--- Test.java
+++ Test.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter 5 numbers:");
+        
+        int smallest = Integer.MAX_VALUE;
+        int biggest = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            int number = scanner.nextInt();
+            
+            if (number < smallest) {
+                smallest = number;
+            }
+            if (number > biggest) {
+                biggest = number;
+            }
+        }
+        
+        scanner.close();
+        
+        System.out.println("\nSmallest number: " + smallest);
+        System.out.println("Biggest number: " + biggest);
     }
 }
